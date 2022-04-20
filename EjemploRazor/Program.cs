f09dfc9acb2b44602148241a058eb50de9fa877f
fran <ctefran@hotmail.com>
2022-04-20 11:40:36 +0200

    Comentario Añadido

--- EjemploRazor/Program.cs
+++ EjemploRazor/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using EjemploRazor.Data;
 using EjemploRazor;
 
-
+// Comentario para GIt
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
